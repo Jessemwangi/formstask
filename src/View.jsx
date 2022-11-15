c89ce3b1cf8c6e5d3454eb2dc7fc99bb1b79bda2
Jessemwangi <jessejzee.com>
2022-11-15 09:49:47 +0200

    fixed css

--- src/View.jsx
+++ src/View.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
+import './View.css'
 
 
 const View = props => {
     return (
-        <div>
-            <h1>this is your input</h1>
+        <div className='view'>
+            <h2>The following Details will be submited</h2>
           <span>FirsName : {props.firstname}</span> <br />
           <span>LastName : {props.lastname}</span> <br />
           <span>Telephone : {props.telephone}</span> <br />
           <span>Role : {props.role}</span><br />
-          <span>Message :{props.messsage} </span><br />
+          <span>Message : {props.messsage} </span><br />
         </div>
     );
 };
-
-
 
 export default View;
